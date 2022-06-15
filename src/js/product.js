3851eb973db4aebed928632603ad69915872cf4a
aalund2013 <ran08001@byui.edu>
2022-06-15 16:48:47 -0600

    dependency install error???

--- src/js/product.js
+++ src/js/product.js
@@ -1,45 +1,34 @@
-let products = [];
-let cart = [];
-function convertToJson(res) {
-  if (res.ok) {
-    return res.json();
-  } else {
-    throw new Error("Bad Response");
-  }
-}
+import ProductData from "./productData.js"
+import ProductDetails from "./productDetails.js"
 
-function setLocalStorage(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
-}
+import { getParam } from "./utils.js";
+
+const productId = getParam("product");
+const dataSource = new ProductData("tents");
+const product = new ProductDetails(productId, dataSource);
+product.init();
+
+// let products = [];
+// let cart = [];
+
+// function setLocalStorage(key, data) {
+//   localStorage.setItem(key, JSON.stringify(data));
+// }
 
 function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
 
 function getCartData() {
-  cart = getLocalStorage("so-cart") || [];
+  return getLocalStorage("so-cart") || [];
 }
-// get tents data
-function getProductsData() {
-  fetch("../json/tents.json")
-    .then(convertToJson)
-    .then((data) => {
-      products = data;
-    });
-}
-// or should we do it this way?
-// async function getProductsDataAwait() {
-//   products = await fetch("../json/tents.json").then(convertToJson);
+
+// // add to cart button event handler
+// function addToCart(e) {
+//   const product = products.find((item) => item.Id === e.target.dataset.id);
+//   cart.push(product);
+//   setLocalStorage("so-cart", cart);
 // }
 
-// add to cart button event handler
-function addToCart(e) {
-  const product = products.find((item) => item.Id === e.target.dataset.id);
-  cart.push(product);
-  setLocalStorage("so-cart", cart);
-}
-
-getProductsData();
 getCartData();
-// add listener to Add to Cart button
-document.getElementById("addToCart").addEventListener("click", addToCart);
+
