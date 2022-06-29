3b85b68c35be55835aad174ab6d4b60c1f18cf2f
CAMILA ROMERO <camilaar@byui.edu>
2022-06-29 10:22:55 -0400

    cr--individual6

--- src/js/productData.js
+++ src/js/productData.js
@@ -11,13 +11,26 @@ function convertToJson(res) {
 export default class ProductData {
   constructor() {
   }
-
+  
   getData(category) {
     return fetch(baseURL + `products/search/${category}`)
                 .then(convertToJson).then((data) => data.Result);
   }
   async findProductById(id) {
-    return await fetch(baseURL + `product/${id}`).then(convertToJson).then((data) => data.Result);
+    return await fetch(baseURL + `product/${id}`)
+    .then(convertToJson)
+    .then((data) => data.Result);
     
+  }
+
+  async checkout(payload) {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    };
+    return await fetch(baseURL + 'checkout/', options).then(convertToJson);
   }
 }
